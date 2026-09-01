964f23c4e21b59b4a63ffb9f45437f0f56a4af67
olmosdev <olmos.develop@gmail.com>
2026-09-01 11:26:45 -0600

    create Repository for the Category

---
+++ EcommerceAPI/EcommerceAPI/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+
+using EcommerceAPI.Models;
+
+namespace EcommerceAPI.Repository.IRepository;
+
+public interface ICategoryRepository
+{
+    ICollection<Category> GetCategories();
+    Category GetCategory(int id);
+    bool CategoryExists(int id);
+    bool CategoryExists(string name);
+    bool CreateCategory(Category category);
+    bool UpdateCategory(Category category);
+    bool DeleteCategory(Category category);
+    bool Save();
+}
